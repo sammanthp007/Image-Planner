fa85e1346c4e6654bd4d153c71fcace220861df6
sammanthp007 <samman.thapa@bison.howard.edu>
2017-04-18 03:00:45 -0400

    addURL start

--- image.js
+++ image.js
@@ -3,12 +3,45 @@
 window.onload = function() {
   $('add_btn').onclick = addImage;
   $('delete_btn').onclick = deleteImage;
-  $('loaded_img').onclick = showURL;
+  $('planning_div').onclick = showURL;
 }
 
 function addImage() {
-  console.log("Clicked add");
+  var inputurl = document.getElementById("input_url").value;
+
+  if (isValidUrl(inputurl)) {
+    addImageToPlannigArea(inputurl);
+  } else {
+    displayError("URL cannot be empty and must be valid");
+  }
 }
+
+function isValidUrl(url) {
+  var exp = "[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)";
+  var regex = new RegExp(exp);
+  return url.match(exp);
+}
+
+function addImageToPlanningArea(url) {
+  // create elemet to place the image
+  var image = document.createElement("img");
+  
+  image.className = "thumbnail_image";
+
+  // add zooming capability
+  image.observe("mouseover", zoomIn);
+  image.observe("mouseout", zoomOut)
+
+  // Image tag is created, then added inside blank area.
+  // the tag is removed when the image fails to load.
+  // Causes an undesired effect on the tag.
+  $("planning_div").appendChild(image);
+}
+
+function displayError(err) {
+  console.log(err);
+}
+
 
 function deleteImage() {
   console.log("Clicked delete image");
